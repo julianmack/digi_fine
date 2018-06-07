2395f8204edf3810857a012862ae1790acacb91c
Julian Mack <julianmack@hotmail.co.uk>
2018-06-07 15:25:51 +0100

    second commit

--- ex1.py
+++ ex1.py
@@ -1,4 +1,7 @@
 """Exercise 1 – Create a list of random integers (5-10 integers). Assign each number with a probability.
 Then randomly generate 100 numbers from the list given the probability distribution you just
 generated. Finally, plot a histogram to show the results (i.e. number of times each number from the
-list is generated)""".
+list is generated)."""
+
+
+"updated text"
