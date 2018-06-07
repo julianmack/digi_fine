3212976351d87d196e88d575f488fb238bba5e98
Julian Mack <julianmack@hotmail.co.uk>
2018-06-07 15:17:34 +0100

    set project structure

---
+++ spec.py
@@ -0,0 +1,28 @@
+"""Data science technical test
+Overview:
+We have devised the following simple test to get a better understanding of your:
+1) Experience of a particular programming language.
+2) Approach to a particular problem.
+3) Style and preference of writing code.
+You are allowed to use one of the two languages (R / Python) and feel free to answer the questions
+in any IDE of your preference. The idea is we will spend some time going through the results in a
+Case Study meeting.
+
+
+Exercise 1 – Create a list of random integers (5-10 integers). Assign each number with a probability.
+Then randomly generate 100 numbers from the list given the probability distribution you just
+generated. Finally, plot a histogram to show the results (i.e. number of times each number from the
+list is generated).
+
+Exercise 2 - Please extract and derive the following information from the Wikipedia page about
+Python (https://en.wikipedia.org/wiki/Python_(programming_language)) and output the following
+items:
+• The name of “designed by” person for the Python programming language according to the
+Wikipedia page.
+• Number of unique words in the ‘History’ session.
+• (Optional) Number of languages influenced by Python according to the Wikipedia page.
+
+
+Exercise 3 – Below is an output from a model predicting a continuous outcome (house price). Please
+take a look and bring your thoughts to the Case Study meeting for discussion.
+"""
