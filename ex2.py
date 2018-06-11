34d9f87226f9e381cff73ebb6493571a23eeaca7
Julian Mack <julianmack@hotmail.co.uk>
2018-06-11 12:54:12 +0100

    "ex2 added"

---
+++ ex2.py
@@ -0,0 +1,18 @@
+"""SPEC: Please extract and derive the following information from the Wikipedia page about
+Python (https://en.wikipedia.org/wiki/Python_(programming_language)) and output the following
+items:
+a) The name of “designed by” person for the Python programming language according to the
+Wikipedia page.
+b) Number of unique words in the ‘History’ session.
+c) (Optional) Number of languages influenced by Python according to the Wikipedia page."""
+
+def main():
+    #check valid input:
+    n = check_input_args()
+
+
+
+
+
+if __name__ == "__main__":
+    main()
