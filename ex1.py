574f0cf51b906b7bbdc821d20ac01131f771b3e8
Julian Mack <julianmack@hotmail.co.uk>
2018-06-11 11:09:50 +0100

    Ready to begin

--- ex1.py
+++ ex1.py
@@ -3,5 +3,17 @@ Then randomly generate 100 numbers from the list given the probability distribut
 generated. Finally, plot a histogram to show the results (i.e. number of times each number from the
 list is generated)."""
 
+import matplotlib.pyplot as plt
+import seaborn as sns
+import numpy as np
 
-"updated text"
+
+np.random.seed(32)
+data = np.random.randn(3,100)
+
+
+sns.set()  # sets look of hist to nicely designed seaborn package
+_ = plt.hist(data, bins=10)
+#always label axes
+
+plt.show()
